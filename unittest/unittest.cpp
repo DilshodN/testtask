d7345bbebeeedf78b5a06bba59a948ec11efe559
DilshodN <dilshod8471@gmail.com>
2024-04-11 13:34:54 +0700

    tests

--- unittest/unittest.cpp
+++ unittest/unittest.cpp
@@ -5,33 +5,29 @@
 #include "../StrategyFactory.h"
 #include "../WordCountStrategy.h"
 
-TEST(abc, s)
-{
-    EXPECT_EQ(1, 1);
+TEST(FileProcessingStrategyTest, WordCountTest) {
+    std::string filename = "test_file.txt";
+    std::ofstream testfile(filename);
+    testfile << "apple banana apple orange apple";
+    testfile.close();
+
+    std::unique_ptr<FileProcessingStrategy> strategy = FileProcessingStrategyFactory::createStrategy(FileProcessingStrategyFactory::Mode::WORD_COUNT);
+    testing::internal::CaptureStdout();
+    strategy->process(filename, "apple");
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Word count for 'apple': 3\n");
 }
-// TEST(FileProcessingStrategyTest, WordCountTest) {
-//     std::string filename = "test_file.txt";
-//     std::ofstream testfile(filename);
-//     testfile << "apple banana apple orange apple";
-//     testfile.close();
 
-//     std::unique_ptr<FileProcessingStrategy> strategy = FileProcessingStrategyFactory::createStrategy(FileProcessingStrategyFactory::Mode::WORD_COUNT);
-//     testing::internal::CaptureStdout();
-//     strategy->process(filename, "apple");
-//     std::string output = testing::internal::GetCapturedStdout();
-//     EXPECT_EQ(output, "Word count for 'apple': 3\n");
-// }
+TEST(FileProcessingStrategyTest, ChecksumTest) {
+    std::string filename = "test_file.bin";
+    std::ofstream testfile(filename, std::ios::binary);
+    uint32_t values[] = { 10, 20, 30, 40 };
+    testfile.write(reinterpret_cast<const char*>(&values), sizeof(values));
+    testfile.close();
 
-// TEST(FileProcessingStrategyTest, ChecksumTest) {
-//     std::string filename = "test_file.bin";
-//     std::ofstream testfile(filename, std::ios::binary);
-//     uint32_t values[] = { 10, 20, 30, 40 };
-//     testfile.write(reinterpret_cast<const char*>(&values), sizeof(values));
-//     testfile.close();
-
-//     std::unique_ptr<FileProcessingStrategy> strategy = FileProcessingStrategyFactory::createStrategy(FileProcessingStrategyFactory::Mode::CHECKSUM);
-//     testing::internal::CaptureStdout();
-//     strategy->process(filename);
-//     std::string output = testing::internal::GetCapturedStdout();
-//     EXPECT_EQ(output, "Checksum: 100\n");
-// }
+    std::unique_ptr<FileProcessingStrategy> strategy = FileProcessingStrategyFactory::createStrategy(FileProcessingStrategyFactory::Mode::CHECKSUM);
+    testing::internal::CaptureStdout();
+    strategy->process(filename);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Checksum: 100\n");
+}
